f493c5f4374fbe35e288435de13488a9a1bf818b
Igor Sobral <igorgmz1421@gmail.com>
2024-03-12 22:40:52 -0300

    adding navbar component

---
+++ components/navbar.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { NAV_LINKS } from "@/constants";
+import Button from "./button";
+
+const Navbar = () => {
+  return (
+    <nav className="flexBetween max-container padding-container  relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+
+      <div className="lg:flexCenter hidden">
+        <Button
+          title="Login"
+          type="button"
+          variant="btn_dark_green"
+          icon="/user.svg"
+        />
+      </div>
+      <Image src={"menu.svg"} alt="menu" width={32} height={32} className="inline-block cursor-pointer lg:hidden" />
+    </nav>
+  );
+};
+
+export default Navbar;
