46299af9090f55cd99518df170420dad77a201f9
Igor Sobral <igorgmz1421@gmail.com>
2024-03-13 16:34:11 -0300

    .

--- components/guide.tsx
+++ components/guide.tsx
@@ -5,7 +5,7 @@ const Guide = () => {
   return (
     <section className="flexCenter flex-col">
       <div
-        className="border-2 border-red-500 padding-container
+        className="padding-container
       max-container w-full pb-24"
       >
         <Image src="/camp.svg" alt="camp" width={50} height={50} />
@@ -33,7 +33,7 @@ const Guide = () => {
 
       <div
         className="flexCenter max-container relative w-full
-      border-2 border-blue-500"
+      "
       >
         <Image
           src="/boat.png"
