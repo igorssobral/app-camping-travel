aad098011f0e943280eab485015c90d433710bb3
Igor Sobral <igorgmz1421@gmail.com>
2024-03-12 22:41:07 -0300

    adding button component

---
+++ components/button.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  variant: string;
+};
+
+const Button = ({ type, icon, title, variant }: ButtonProps) => {
+  return (
+    <button
+    className={`flexCenter gap-3 rounded-full border ${variant}`}
+    type={type}>
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap">{title}</label>
+    </button>
+  );
+};
+
+export default Button;
