566d9965a05ab8c8dac1a58dbf936af19b1e0d19
Igor Sobral <igorgmz1421@gmail.com>
2024-03-22 20:54:02 -0300

    remove border

--- components/features.tsx
+++ components/features.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Features = () => {
   return (
     <section
-      className="border-2 border-red-500 flex flex-col flexCenter
+      className="flex flex-col flexCenter
     overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24"
     >
       <div
